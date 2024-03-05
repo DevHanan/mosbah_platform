ffcca456a0fbeb558c66d2ac4b35be5c4a15021c
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-03-05 23:32:40 +0200

    first commit

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -88,9 +88,9 @@ class StudentController extends Controller
     public function ExportToExcel(){
 
         // return "success";
-        return $this->okApiResponse('', __('Student deleted successfully'));
+        // return $this->okApiResponse('', __('Student deleted successfully'));
 
-        // return Excel::download(new StudentsExport, 'students.xlsx');
+         return Excel::download(new StudentsExport, 'students.xlsx');
 
     }
 }

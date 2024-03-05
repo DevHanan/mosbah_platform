1233bbd23c60865f0da34795f5f3653dd8316602
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-03-05 22:13:12 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    Artisan::call('optimize:clear');
 });

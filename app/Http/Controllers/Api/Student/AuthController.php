1f2d7c5631ab25fbf08e13698a51b2533ba801a1
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-03-28 02:06:19 +0200

    add Instructor & student register

--- app/Http/Controllers/Api/Student/AuthController.php
+++ app/Http/Controllers/Api/Student/AuthController.php
@@ -49,8 +49,6 @@ class AuthController extends Controller
 
         
         $user = Student::create($request->except('password'));
-        $token =$this->generateToken($user);
-        $user->api_token = $token->plainTextToken;
         $user->password = Bcrypt($request->password);
         $user->active = '0';
         $user->save();

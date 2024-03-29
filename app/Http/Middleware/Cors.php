b5d8f736711c23fc83cbbd5e7e67de0320a51d87
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-03-30 00:44:36 +0200

    add course & cours details api

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,11 +15,11 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
+    public function handle($request, Closure $next) {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type');
+          ->header('Access-Control-Allow-Origin', '*')
+          ->header('Access-Control-Allow-Methods', '*')
+          ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+          ->header('Access-Control-Allow-Credentials',' true');
     }
 }

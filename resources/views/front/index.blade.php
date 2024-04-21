653dbbdc453f9b1fc95fb519c58fe2a22ec6c46b
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-04-21 21:00:34 +0200

    add home page and solve bug

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dwafeer</title>
     <link rel="stylesheet" href="public/front/css/bootstrap.min.css">

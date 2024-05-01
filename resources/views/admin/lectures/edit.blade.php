2adcbc19818d663f9aba4d9568663bbf8f029457
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-01 09:43:27 +0300

    update

--- resources/views/admin/lectures/edit.blade.php
+++ resources/views/admin/lectures/edit.blade.php
@@ -98,7 +98,7 @@
               </div>
               <div class="mb-3">
               <label class="form-label">{{ __('admin.lectures.descriptions') }} <span class="form-label-description"></span></label>
-                                <textarea class="form-control" name="descriptions" rows="6" placeholder="Content..">
+                                <textarea class="form-control" name="description" rows="6" placeholder="Content..">
                                 {{ $row->description }}
 
                                 </textarea>

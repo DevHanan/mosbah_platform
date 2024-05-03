f503dd6b3b0f6398ab80463c8e987b1135e779c6
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-03 07:06:55 +0300

    add favicon

--- resources/views/front/sign_step2.blade.php
+++ resources/views/front/sign_step2.blade.php
@@ -7,12 +7,13 @@
   {{ $title }} -
   @endif
   {{ $setting->title }}</title>
-  <link rel="shortcut icon" href="{{ asset($setting->favicon_path) }}">    <link rel="stylesheet" href="{{asset('public/front/css/bootstrap.min..css')}}">
+  <link rel="shortcut icon" href="{{ asset($setting->favicon_path) }}">  
+    <link rel="stylesheet" href="{{asset('public/front/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
-    <link rel="stylesheet" href="{{asset('public/front/css/swiper-bundle.min..css')}}">
-    <link rel="stylesheet" href="{{asset('public/front/css/custom..css')}}">
-    <link rel="stylesheet" href="{{asset('public/front/css/media..css')}}">
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="{{asset('public/front/css/swiper-bundle.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/custom.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/media.css')}}">
+    <link rel="stylesheet" href="public/front/styles.css" />
 </head>
 <body>
     <div class="register sign_step2">

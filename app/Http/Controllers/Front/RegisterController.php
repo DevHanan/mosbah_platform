1e113bca8d539acb5b085a68964edaa176836d95
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-03 07:48:06 +0300

    add Form Reuest

--- app/Http/Controllers/Front/RegisterController.php
+++ app/Http/Controllers/Front/RegisterController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
+use Bcrypt;
 
 class RegisterController extends Controller
 {
     
     public function registerStudent(RegisterRequest $request){
+        $type= $request->input('type');
+        $model =  $type  == 'instructor' ? 'Instructor' : 'Student';
+        $request->merge(['password'=> Bcrypt($request->password)]);
+        $item = $model::create($request->all());
+        Toastr::success(__('admin.msg_created_successfully'), __('admin.msg_success'));
+        return redirect('/sign_step2?type='.$type,compact(['item']));
 
-        return $request->all();
+ 
+
     }
 }

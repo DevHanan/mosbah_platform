ceb247d29fe0e367539b0aac6a92a6b9d1757c4b
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-03 06:41:57 +0300

    add favicon

--- resources/views/front/layouts/common/header_script.blade.php
+++ resources/views/front/layouts/common/header_script.blade.php
@@ -6,6 +6,8 @@
   {{ $title }} -
   @endif
   {{ $setting->title }}</title>
+  <link rel="shortcut icon" href="{{ asset($setting->favicon_path) }}">
+
     <link rel="stylesheet" href="{{asset('public/front/css/bootstrap.min.css')}}">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> <!--animation-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>

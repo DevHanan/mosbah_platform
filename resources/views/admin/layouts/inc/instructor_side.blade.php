ad17a8bec0d23cef69ae6fb5e46de68f213efdb4
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-14 19:01:17 +0300

    updates

--- resources/views/admin/layouts/inc/instructor_side.blade.php
+++ resources/views/admin/layouts/inc/instructor_side.blade.php
@@ -158,8 +158,8 @@
     </div>
     <div class="collapse navbar-collapse" id="sidebar-menu">
       <ul class="navbar-nav pt-lg-3">
-        <li class="nav-item @if(request()->routeIs('admin.dashboard')) active @endif">
-          <a class="nav-link @if(request()->routeIs('admin.dashboard')) active @endif" href="{{url('admin/dashboard')}}">
+        <li class="nav-item >
+          <a class="nav-link  href="{{url('admin/dashboard')}}">
             <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />

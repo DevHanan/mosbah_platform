ba8b06f5da29fe174163e22205610540d283e21f
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-19 20:45:09 +0300

    updates

--- resources/views/front/layouts/common/header_script.blade.php
+++ resources/views/front/layouts/common/header_script.blade.php
@@ -6,7 +6,7 @@
   {{ $title }} -
   @endif
   {{ $setting->title }}</title>
-  <link rel="shortcut icon" href="{{ asset($setting->favicon_path) }}">
+  <link rel="shortcut icon" href="{{ asset($setting->iconFullPath) }}">
 
     <link rel="stylesheet" href="{{asset('public/front/css/bootstrap.min.css')}}">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> <!--animation-->

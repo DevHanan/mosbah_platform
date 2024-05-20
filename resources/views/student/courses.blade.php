666e48d922ea5921d3eb34578a7eacb0181bd48e
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-21 02:02:25 +0300

    updates

--- resources/views/student/courses.blade.php
+++ resources/views/student/courses.blade.php
@@ -243,7 +243,7 @@
                     </div>
 
                     <div class="row" id="paginationCard">
-                        @foreach($list_courses as $course)
+                        @foreach($courses as $course)
                         <div class="col-sm-6 card_pagination">
                             <article class="rounded mt-4 shadow">
                                 <div class="row">

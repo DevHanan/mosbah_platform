971b506eafbc720a357e0c0c1a2ea079c72e5174
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-21 01:05:06 +0300

    updates

--- resources/views/admin/layouts/inc/admin_side.blade.php
+++ resources/views/admin/layouts/inc/admin_side.blade.php
@@ -6,7 +6,7 @@
     </button>
     <h1 class="navbar-brand navbar-brand-autodark">
       <a href="{{url('/')}}">
-        <img src="{{ asset($setting->logo_path)}}" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+        <img src="{{ asset($setting->logoFullPath)}}" width="110" height="32" alt="Tabler" class="navbar-brand-image">
       </a>
     </h1>
     <div class="navbar-nav flex-row d-lg-none">

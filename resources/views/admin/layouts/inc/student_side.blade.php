c1a763b60ee9b3df86036c6730b5c2b5434cb5c2
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-21 07:55:18 +0300

    add script

--- resources/views/admin/layouts/inc/student_side.blade.php
+++ resources/views/admin/layouts/inc/student_side.blade.php
@@ -266,7 +266,7 @@
           </a>
         </li>
         <li class="nav-item @if(request()->routeIs('student.externalCertifications')) active @endif" ">
-          <a class="nav-link " href="{{url('student/external-certifications')}}">
+          <a class="nav-link " href="{{url('student/externel-certifications')}}">
             <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />

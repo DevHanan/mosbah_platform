4b4fe0898c31a78d091d4b9147db7cf2e9eaa4a2
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-21 08:25:52 +0300

    add script

--- resources/views/student/certifications/create.blade.php
+++ resources/views/student/certifications/create.blade.php
@@ -5,7 +5,7 @@
     <div class="container-xl">
         <div class="row g-2 align-items-center">
             <div class="col">
-            {{ Breadcrumbs::render('add-externalCertifications') }}
+                {{ Breadcrumbs::render('add-externalCertifications') }}
 
             </div>
             <!-- Page title actions -->
@@ -69,37 +69,42 @@
                         </div>
 
                         <div class="form-group col-md-6">
-    <label class="form-label" for="track_id">{{__('admin.certifications.track_name')}} <span>*</span></label>
-    <select class="form-control select2" name="track_id" id="track_id">
-      <option value="">{{ __('select') }}</option>
-      @foreach( $tracks as $track )
-      <option value="{{ $track->id }}" @if(old('track_id')==$track->id) selected @endif>{{ $track->name }}</option>
-      @endforeach
-    </select>
+                            <label class="form-label" for="track_id">{{__('admin.certifications.track_name')}} <span>*</span></label>
+                            <select class="form-control select2" name="track_id" id="track_id">
+                                <option value="">{{ __('select') }}</option>
+                                @foreach( $tracks as $track )
+                                <option value="{{ $track->id }}" @if(old('track_id')==$track->id) selected @endif>{{ $track->name }}</option>
+                                @endforeach
+                            </select>
 
-    @error('track_id')
-    <div class="invalid-feedback">
-      {{ $message }}
-    </div>
-    @enderror
-  </div>
+                            @error('track_id')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
 
                         <div class="form-group col-md-6">
-              <label class="form-label" for="course_id">{{__('admin.certifications.course_name')}} <span>*</span></label>
-              <select class="form-control select2" name="course_id" id="course_id">
-                <option value="">{{ __('select') }}</option>
-                @foreach( $courses as $course )
-                <option value="{{ $course->id }}" @if(old('course_id')==$course->id) selected @endif>{{ $course->name }}</option>
-                @endforeach
-              </select>
+                            <label class="form-label" for="course_id">{{__('admin.certifications.course_name')}} <span>*</span></label>
+                            <select class="form-control select2" name="course_id" id="course_id">
+                                <option value="">{{ __('select') }}</option>
+                                @foreach( $courses as $course )
+                                <option value="{{ $course->id }}" @if(old('course_id')==$course->id) selected @endif>{{ $course->name }}</option>
+                                @endforeach
+                            </select>
 
-              @error('course_id')
-              <div class="invalid-feedback">
-                {{ $message }}
-              </div>
-              @enderror
-            </div>
-                        <div class="form-group col-md-6">
+                            @error('course_id')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">{{ __('admin.certifications.notes') }} <span class="form-label-description"></span></label>
+                            <textarea dir="auto" class="form-control" name="notes" rows="4" placeholder="Content.."></textarea>
+                        </div>
+
+                        <div class="form-group col-md-12">
 
 
                             <label for="logo">{{ __('admin.certifications.file') }}</label>

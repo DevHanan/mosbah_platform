602cc78eb77cf768ac80794038b128fc807a5563
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-21 09:03:51 +0300

    add script

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -9,57 +9,8 @@
 
       <div class="col-12">
         <div class="row row-cards">
-          <div class="col-sm-6 col-lg-3">
-            <div class="card card-sm">
-              <div class="card-body" style="min-height:90px;">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <span class="bg-primary text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/currency-dollar -->
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-school">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
-                        <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
-                      </svg> </span>
-                  </div>
-                  <div class="col">
-                    <div class="font-weight-medium">
-                      {{ $students->count() }}
-                    </div>
-                    <div class="text-secondary">
-                      {{ __('admin.dashboard.student_number')}}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-lg-3">
-            <div class="card card-sm">
-              <div class="card-body" style="min-height:90px;">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <span class="bg-green text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-community" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
-                        <path d="M13 7l0 .01" />
-                        <path d="M17 7l0 .01" />
-                        <path d="M17 11l0 .01" />
-                        <path d="M17 15l0 .01" />
-                      </svg> </span>
-                  </div>
-                  <div class="col">
-                    <div class="font-weight-medium">
-                      {{ $instructors->count() }}
-                    </div>
-                    <div class="text-secondary">
-                      {{ __('admin.dashboard.instructor_number')}}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        
+         
           <div class="col-sm-6 col-lg-3">
             <div class="card card-sm">
               <div class="card-body" style="min-height:90px;">

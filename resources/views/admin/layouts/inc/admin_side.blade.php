316923573e84dc62d2e4bc75e4f72dbdbd101341
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-24 03:59:00 +0300

    add script

--- resources/views/admin/layouts/inc/admin_side.blade.php
+++ resources/views/admin/layouts/inc/admin_side.blade.php
@@ -378,15 +378,15 @@
             <div class="dropdown-menu-columns">
               <div class="dropdown-menu-column">
                 <a class="dropdown-item @if(request()->routeIs(['admin.certifications'])) active @endif" href="#">
-                {{ __('navbar.certifications.add') }}
+                {{ __('navbar.certifications.list_dwafer') }}
 
                 </a>
                 <a class="dropdown-item @if(request()->routeIs(['admin.student-certifications.*'])) active @endif" href="#">
-                {{ __('navbar.certifications.add') }}
+                {{ __('navbar.certifications.student_certifications') }}
 
                 </a>
                 <a class="dropdown-item @if(request()->routeIs(['admin.student-certifications.*'])) active @endif" href="#">
-                  {{ __('navbar.certifications.add') }}
+                  {{ __('navbar.certifications.student_certifications_from_out') }}
 
                 </a>
                 <a class="dropdown-item @if(request()->routeIs(['admin.certifications.create'])) active @endif" href="#">

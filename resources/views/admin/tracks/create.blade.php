dce11588ac96e3e3a20f3176c03c669d8032d17e
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-26 12:41:07 +0300

    updates

--- resources/views/admin/tracks/create.blade.php
+++ resources/views/admin/tracks/create.blade.php
@@ -36,7 +36,7 @@
 
                         <div class="col-md-12">
                             <label class="form-label" for="title"> {{__('admin.tracks.name')}} <span>*</span></label>
-                            <input type="text" class="form-control" name="name" id="title" value="{{ old('title') }}" required>
+                            <input type="text" class="form-control" name="name" id="title" value="{{ old('name') }}" required>
 
                             @error('name')
                             <div class="invalid-feedback">

b66cdc492af3afd67fb774a4cf6688ab7b249c4a
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-26 13:53:47 +0300

    updates

--- resources/views/admin/layouts/common/footer_script.blade.php
+++ resources/views/admin/layouts/common/footer_script.blade.php
@@ -26,7 +26,7 @@
         <script>
 			    $(document).ready(function(){
             ClassicEditor
-                .create(document.querySelector('.richtext'))
+                .create(document.querySelector('textarea'))
                 .catch(error => {
                     console.error(error);
                 });

23d4e86f1dee5f8bf0089f77e66ae99486546edf
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-26 14:10:51 +0300

    updates

--- resources/views/front/lecture.blade.php
+++ resources/views/front/lecture.blade.php
@@ -33,7 +33,7 @@
                                     <p> <img src="{{asset('public/front/img/icons/fi-rr-e-learning.png')}}" class="ms-2" width="20" alt=""> <span> الدورة : {{ optional($lecture->course)->name }} </span></p>
                                     <p> <img src="{{asset('public/front/img/icons/fi-rr-e-learning.png')}}" class="ms-2" width="20" alt=""> <span> المرحلة : {{ optional($lecture->level)->name }} </span></p>
                                     <p> <img src="{{asset('public/front/img/icons/fi-rr-calendar.png')}}" class="ms-2" width="20" alt=""> <span> {{ $lecture->created_at->format('d/m/Y') }} </span></p>
-                                    <p> <img src="{{asset('public/front/img/icons/fi-rr-time-quarter-to.svg')}}" class="ms-2" width="20" alt=""> <span> {{ $lecture->period }} </span></p>
+                                    <p> <img src="{{asset('public/front/img/icons/fi-rr-time-quarter-to.svg')}}" class="ms-2" width="20" alt=""> <span> {{ $lecture->period }} ساعة </span></p>
                                     <p> <img src="{{asset('public/front/img/icons/fi-rr-e-learning.png')}}" class="ms-2" width="20" alt=""> <span>
                                             نوع المحاضرة :
                                             @if($lecture->type == 1)

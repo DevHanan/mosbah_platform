d2970cb9ba61a2b8c961adb6a95a72c6ec50ff5b
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-27 18:26:13 +0300

    update

--- resources/lang/en/navbar.php
+++ resources/lang/en/navbar.php
@@ -1,33 +1,44 @@
 <?php
 return [
-    "Home" => "Home",
-    'users' => "Users",
-    "user_managment" => "Users Management",
-    "roles" => "Roles & Permissions",
-    "countries" => "Countries",
-    "paymenttypes" => "Payment Types",
-    "master_data"  => "General Data",
+    "Home" => "الرئيسية",
+    'users' => "المستخدمين",
+    'add_user' => "إضافة مستخدم",
+    "user_managment" => "المستخدمين",
+    "countries" => "الدول",
+    "paymenttypes" => "أنواع الدفع ",
+    "master_data"  => "الإعدادت",
     'students' => [
-        "student_managment"  => "Students managment",
-        "list" => "List students",
-        "add"   => "New student",
+        "student_managment"  => "إدارة الطلاب",
+        "list" => "قائمة الطلاب",
+        "add"   => "إضافة طالب"
+    ],
+    'users' => [
+        "list" => "المستخدمين",
+        "add"   => "إضافة مستخدم"
+    ],
+    'roles' => [
+        "list" => "الأدوار",
+        "add"   => "إضافة دور",
+        'show' => 'عر تفاصيل الدور'
     ],
 
     'instructors' => [
-        "instructor_management"  => "Instructors Management",
-        "list" => "List Instructors",
-        "add"   => "New Instructor"
+        "instructor_management"  => "إدارة المدربين",
+        "list" => "قائمة المدربين",
+        "add"   => "إضافة محاضر"
     ],
     'subscriptions' => [
-        'title'  => 'Student subscriptions',
-        "list" => "List subscriptions",
-        "add"  => " New subscription "
+        'title'  => 'اشتراكات الطلاب',
+        "list" => "اشتراكات الطلاب بالدورات",
+        "add"  => "إضافة اشتراك طالب بدورة"
 
     ],
     'coupons' => [
         "coupons_management"  => "إدارة الكوبونات",
-        "list" => "عرض الكوبونات",
-        "add"   => "إضافة كوبون"
+        "list" => "قائمة الكوبونات",
+        "add"   => "إضافة كوبون",
+        "edit"   => "تعديل كوبون",
+
     ],
     'certifications' => [
         "certifications_management" => " الشهادات",
@@ -37,34 +48,137 @@ return [
         'student_certifications_from_out'  => 'شهادات الطلاب من جهات خارجية'
     ],
     'tickets' => [
-        "tickets_management"  => "Ticket Managment",
-        "list" => "List Tickets",
-        "add"   => "New",
-        "instrutor_msg" => "Instructor Message",
-        "student_msg" => "Student Message"
+        "tickets_management"  => " الدعم",
+        "list" => "عرض الدعم الفنى",
+        "add"   => "",
+        "instrutor_msg" => "مراسلات المدربين",
+        "student_msg" => "مراسلات الطلاب",
+        'visitors'  => 'مراسلات الزوار',
+        'notifications'  => 'إشعارات',
+
     ],
     "courses" => [
-        "title"  => "Courses",
-        "list_tracks" => "List Tracks",
-        "add_track" => "New Track",
-        "list_course_types"  => "List Course Type",
-        "add_course_types" => "New Type",
-        "list_lectures"  => "List Lectures",
-        "add_lecture" => " New Lecture",
-        "list_levels" => "List Levels",
-        "add_level" => "New Level",
-        "add_course"  => "New Course",
-        'recommened_courses' => 'Recommend Courses'
+        'start_soon_courses' => 'دورات تبدا قريبا',
+        'edit_course' => 'تعديل بيانات الدورة',
+        'all_courses' => 'كل الدورات',
+        "title"  => "الدورات التدريبية",
+        "list_tracks" => "المسارات",
+        "add_track" => " إضافه مسار",
+        "list_course_types"  => "أنواع الدورات",
+        "add_course_types" => "إضافة نوع",
+        "list_lectures"  => "عرض المحاضرات",
+        "add_lecture" => " إضافه محاضره",
+        'edit_lecture' => 'تعديل محاضرة',
+        "list_levels" => "عرض المستويات",
+        "add_level" => "إضافه مستوي",
+        "add_course"  => "إضافة دوره",
+        'recommened_courses' => 'دورات مرشحة',
+        'reviews'  => 'ريفيوهات الدورات',
+        "list_courses" => "الدورات التدريبية",
+
+
 
 
     ],
     'settings' => [
-        'general_settings' => 'General Setting',
-        'policies' => 'Policies & Conditions',  
-        'about_us_settings' => 'About Us',
-        'questions' =>'Frequent asked questions',
-        'teams' => 'Our Team ',
-        'parteners' => 'Success Partener'
+        'general_settings' => 'إعدادت عامة ',
+        'policies' => 'السياسات',  
+        'about_us_settings' => 'من نحن',
+        'questions' =>'الأسئلة المتكررة',
+        'teams' => 'فريق العمل ',
+        'parteners' => 'الشركاء والداعمين',
+        'language_setting' => 'إعدادات اللغة',
+        'payment_settings' => 'إعدادات وسائل الدفع',
+        'zoom_settings' => 'اعدادات زووم',
+        'home_settings' => 'اعدادات الصفحة الرئيسية',
+        'contactus_settings' => 'تواصل معنا',
+        'reviews' =>'التقييمات والآراء'
 
-    ]
+    ],
+    'parteners' => [
+        'list' => 'عرض الشركاء والداعمين',
+        'add' => 'إضافة شريك جديد',
+    ],
+    'teams' => [
+        'list' => 'عرض فريق العمل',
+        'add' => 'إضافة عضو جديد',
+    ],
+    'exams'  => [
+        'exam_management' => 'الاختبارات',
+        'quections_list' => 'قائمة بنوك الأسئلة',
+        'add_question'   => 'إضافة بنك أسئلة',
+        'list_exam' => 'قائمة الاختبارات ',
+        'add_exam' => 'إضافة اختبار',
+        'exam_result' => 'نتائج اختبارات الطلاب',
+        'exam_setting' => 'اعدادات الاختبارات',
+
+    ],
+    'finances' => [
+        'finances_management' => 'الماليات',
+        'course_profit' => 'ايرادات الدورات',
+        'students_payment' => 'مدفوعات الطلاب',
+        'instructors_profit' => 'أرباح المدربين',
+        'instructors_request' => 'طلبات سحب المدربين',
+        'instructor_paid' => 'مدفوعات أرباح المدربين',
+
+    ],
+    'processing_account' => [
+        'processing_account_management' => 'حساب المعادلة',
+        'course_profit' => 'قائمة المواد',
+        'students_payment' => 'اعدادات حساب المعادلة'
+
+    ],
+    'cvmaker' => [
+        'cvmaker_management' => 'صانع السيرة الذاتية',
+        'cvmaker_template' => 'قوالب السيرة الذاتية ',
+        'add_cvmaker_template' => 'إضافة قالب'
+
+    ],
+    'instructors_side'=>[
+        'Home' => 'الرئيسية',
+        'profile' => 'ملفى الشخصى',
+        'mystudents' => 'طلابي',
+        'mycourses' => 'دوراتي',
+        'tests' => 'الاختبارات',
+        'studentTestResult' => 'نتائج اختبارات الطلاب',
+        'profits' => 'أرباحي',
+        'profits_requests' => 'طلبات سحب الأرباح',
+        'paid_profits' => 'مدفوعات الارباح',
+        'add_profits_requests' => 'إضافة طلب سحب',
+        'paid_profits_requests' => 'مدفوعات المحاضرين'
+
+    ],
+    'students_side'=>[
+        'Home' => 'الرئيسية',
+        'profile' => 'ملفى الشخصى',
+        'mycourses' => 'الدورات المشترك بها',
+        'tests' => 'الاختبارات',
+        'courses_payment' => 'مدفوعات الدورات',
+        'certificationfromin' => 'شهاداتي من المنصة',
+        'certificationfromout' => 'شهاداتي من جهات خارجية',
+        'studentTestResult' => 'نتائج الاختبارات'
+
+    ],
+    'payments' =>[
+        'transactionnumber' => 'رقم العملية',
+        'transcation_date' => 'تاريخ العملية',
+        'course_name' => 'اسم الدورة',
+        'paid_amount' => ' القيمة المدفوعة',
+        'payment_attachment' => 'مرفق اثبات الدفع',
+        'list' => 'سجل مدفوعات الطالب'
+  
+      ],
+      'certifications' => [
+        'name' => 'إسم الشهادة',
+        'course_name' => 'إسم الدورة',
+        'track_name' => 'إسم المسار',
+        'platform_certification' => 'شهادات منصة دوافير ',
+        'externel_certification' => 'شهادات خارجية',
+        'add' => 'إضافة شهادة'
+      ],
+      'cvs' => [
+        'list' => 'قوالب السيرة الذاتية',
+        'add' => 'إضافة قالب جديد',
+        'edit' => 'تعديل قالب'
+    ],
 ];

cb249e7b026a86ff168d0b7a05d626aa39d9aa3b
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-28 18:31:49 +0300

    updates

--- resources/views/front/layouts/common/header_script.blade.php
+++ resources/views/front/layouts/common/header_script.blade.php
@@ -8,12 +8,12 @@
   {{ $setting->title }}</title>
   <link rel="shortcut icon" href="{{ asset($setting->iconFullPath) }}">
 
-    <link rel="stylesheet" href="{{asset('front/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/bootstrap.min.css')}}">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> <!--animation-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
-    <link rel="stylesheet" href="{{asset('$aboutsetting->front/css/swiper-bundle.min.css')}}">
-    <link rel="stylesheet" href="{{asset('front/css/select2.min.css')}}">
-    <link rel="stylesheet" href="{{asset('front/css/select2-bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('front/css/custom.css')}}">
-    <link rel="stylesheet" href="{{asset('front/css/media.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/swiper-bundle.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/select2-bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/custom.css')}}">
+    <link rel="stylesheet" href="{{asset('public/front/css/media.css')}}">
 </head>

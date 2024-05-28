f90e4cd8fd0f35f1125b50861dad0d2b705a234a
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-28 18:02:02 +0300

    updates

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="col-md-6">
                     <div class="info position-relative py-5">
-                        <img src="public/front/img/completed-bg.svg" class="position-absolute img-fluid" alt="">
+                        <img src="{{asset('/front/img/completed-bg.svg')}}" class="position-absolute img-fluid" alt="">
                         <h4 class="title py-5 pb-1 fw-bold secondary-color"> {{ $setting-> title}} </h4>
                         <div class="content fw-bold text-white">
                             {!! $setting->description !!}
@@ -30,18 +30,18 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="img">
-                        <img src="public/front/img/achievement.png" class="img-fluid" alt="">
+                        <img src="{{asset('front/img/achievement.png')}}" class="img-fluid" alt="">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="info">
-                        <h2 class="position-relative"> إنجازاتنا <img src="public/front/img/text-line2.svg" class="position-absolute" alt=""></h2>
+                        <h2 class="position-relative"> إنجازاتنا <img src="{{asset('front/img/text-line2.svg')}}" class="position-absolute" alt=""></h2>
                         <h5 class="my-5 mb-5 ">خلال فترة قصيرة حققنا هذه الإنجازات، نحن بكم ولكم وفي خدمتكم على الدوام.</h5>
                         <div class="row">
 
                             <div class="col-6">
                                 <div class="data mb-4 d-flex align-items-center">
-                                    <div class="img rounded p-3" style="background-color: #E9F8F3;"><img src="public/front/img/icons/GraduationCap.svg" alt=""></div>
+                                    <div class="img rounded p-3" style="background-color: #E9F8F3;"><img src="{{asset('/front/img/icons/GraduationCap.svg')}}" alt=""></div>
                                     <div class="mx-3 text-center">
                                         <h3 class="fw-bold count" data-target="300"> {{ $setting->course_number}}</h3>
                                         <p class="fw-bold">دورات</p>
@@ -51,7 +51,7 @@
 
                             <div class="col-6">
                                 <div class="data mb-4 d-flex align-items-center">
-                                    <div class="img rounded p-3" style="background-color: #FFFAF5;"><img src="public/front/img/icons/VideoCamera.svg" alt=""></div>
+                                    <div class="img rounded p-3" style="background-color: #FFFAF5;"><img src="{{asset('/front/img/icons/VideoCamera.svg')}}" alt=""></div>
                                     <div class="mx-3 text-center">
                                         <h3 class="fw-bold count" data-target="5000">{{ $setting->lecture_number}}</h3>
                                         <p class="fw-bold">محاضرات</p>
@@ -61,7 +61,7 @@
 
                             <div class="col-6">
                                 <div class="data mb-4 d-flex align-items-center">
-                                    <div class="img rounded p-3" style="background-color: #F0F7FF;"><img src="public/front/img/icons/UsersThree.svg" alt=""></div>
+                                    <div class="img rounded p-3" style="background-color: #F0F7FF;"><img src="{{asset('/front/img/icons/UsersThree.svg')}}" alt=""></div>
                                     <div class="mx-3 text-center">
                                         <h3 class="fw-bold count" data-target="50">{{ $setting->instructor_number}}</h3>
                                         <p class="fw-bold">مدربين</p>
@@ -71,7 +71,7 @@
 
                             <div class="col-6">
                                 <div class="data mb-4 d-flex align-items-center">
-                                    <div class="img rounded p-3" style="background-color: #FFEEF0;"><img src="public/front/img/icons/GraduationCap2.svg" alt=""></div>
+                                    <div class="img rounded p-3" style="background-color: #FFEEF0;"><img src="{{asset('/front/img/icons/GraduationCap2.svg')}}" alt=""></div>
                                     <div class="mx-3 text-center">
                                         <h3 class="fw-bold count" data-target="1000">{{ $setting->student_number}}</h3>
                                         <p class="fw-bold">طلاب</p>
@@ -89,7 +89,7 @@
 
     <div class="team trainers py-5">
         <div class="container">
-            <h2> تعرف على <span class="position-relative primary-color"> فريقنا <img src="public/front/img/text-line2.svg" style="bottom: -12px;right: 0;" class="position-absolute" alt=""></span> </h2>
+            <h2> تعرف على <span class="position-relative primary-color"> فريقنا <img src="{{asset('/front/img/text-line2.svg')}}" style="bottom: -12px;right: 0;" class="position-absolute" alt=""></span> </h2>
             <h5 class="my-5 mb-5 "> فريق عمل يضم نخبة من المتميزين فى إدارة المحتوى التعليمى الالكتروني وتقديمه بصورة متكاملة ومتميزة.</h5>
         </div>
         <div class="card__container swiper mt-4">
@@ -130,13 +130,13 @@
     @endif
 
     <div class="aim position-relative my-5">
-        <!-- <img src="public/front/img/aim-bg.svg" class="position-absolute img-fluid" alt=""> -->
+        <!-- <img src="{{asset('/front/img/aim-bg.svg')}}" class="position-absolute img-fluid" alt=""> -->
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
                     <div class="h-100 d-flex flex-column justify-content-center">
                         <h2 class="position-relative"> {{ $setting->mission_title }}
-                            <img src="public/front/img/text-line2.svg" class="position-absolute" alt="">
+                            <img src="{{asset('/front/img/text-line2.svg')}}" class="position-absolute" alt="">
                         </h2>
                         <h5 class="my-5 mb-5 ">
                             {!! $setting->mission_description !!}
@@ -158,11 +158,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 d-none d-md-block">
-                    <div class="img"><img src="public/front/img/messages.svg" class="img-fluid" data-aos="fade-left" data-aos-duration="1000" alt=""></div>
+                    <div class="img"><img src="{{asset('/front/img/messages.svg')}}" class="img-fluid" data-aos="fade-left" data-aos-duration="1000" alt=""></div>
                 </div>
                 <div class="col-md-6">
                     <div class="h-100">
-                        <h2 class="position-relative"> رسالتنا وأهدافنا <img src="public/front/img/text-line2.svg" class="position-absolute" alt=""></h2>
+                        <h2 class="position-relative"> رسالتنا وأهدافنا <img src="{{asset('/front/img/text-line2.svg')}}" class="position-absolute" alt=""></h2>
                         <div class="row mt-5">
                             <div class="col-md-6">
                                 <div class="data mb-5">
@@ -206,7 +206,7 @@
 
     <div class="partners position-relative my-5">
         <div class="container text-center mb-3">
-            <h2 class="position-relative"> شركاء نجاحنا <img src="public/front/img/text-line2.svg" class="position-absolute" style="bottom: -19px;right: 50%;
+            <h2 class="position-relative"> شركاء نجاحنا <img src="{{asset('/front/img/text-line2.svg')}}" class="position-absolute" style="bottom: -19px;right: 50%;
                     transform: translateX(10px);" alt=""></h2>
         </div>
 

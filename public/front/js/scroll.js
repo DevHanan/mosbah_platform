b6fb9d050ae2ad2428e35488d4d8ce8edd002375
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-05-28 19:25:59 +0300

    updates

---
+++ public/front/js/scroll.js
@@ -0,0 +1,19 @@
+
+
+// scroll
+let btn = document.querySelector('.scroll');
+window.onscroll = function(){
+  if(window.scrollY >= 1000){
+      btn.style.display = 'block';
+  }
+  else{
+      btn.style.display = 'none';
+  }
+  btn.onclick = function(){
+      window.scrollTo ({
+          top:0,
+          left:0,
+          behavior:"smooth"
+      })
+  }
+};

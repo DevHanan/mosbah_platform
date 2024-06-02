899290d1252ccf56021b3761ec67d98dd780b381
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-02 17:57:48 +0300

    updates

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -14,7 +14,7 @@
   <div class="page">
 
     @include('admin.layouts.inc.navbar')
-  
+
 
     <div class="page-wrapper">
       @yield('content')
@@ -23,7 +23,27 @@
 
 
 
+  <footer class="footer footer-transparent d-print-none">
+    <div class="container-xl">
+      <div class="row text-center align-items-center flex-row-reverse">
 
+        <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+          <ul class="list-inline list-inline-dots mb-0">
+            <li class="list-inline-item">
+              Copyright © 2023
+              <a href="." class="link-secondary">Tabler</a>.
+              All rights reserved.
+            </li>
+            <li class="list-inline-item">
+              <a href="./changelog.html" class="link-secondary" rel="noopener">
+                v1.0.0-beta20
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
   @include('admin.layouts.common.footer_script')
   @stack('scripts')
 

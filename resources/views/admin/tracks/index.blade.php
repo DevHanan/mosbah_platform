33733513352a25b3587d7c37ac127a96f34d04b7
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-11 01:25:19 +0300

    update contactus

--- resources/views/admin/tracks/index.blade.php
+++ resources/views/admin/tracks/index.blade.php
@@ -81,11 +81,11 @@
 
 
 
-                    <a href="{{ route($route.'.edit',$row->id) }}" class="btn btn-icon btn-primary btn-sm" title="Edit">
+                    <a href="{{ route($route.'.edit',$row->id) }}" class="btn btn-icon btn-primary btn-sm" title="{{__('admin.edit')}}">
                       <span class="far fa-edit "></span>
                     </a>
 
-                    <button type="button" class="btn btn-icon btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{$row->id }}">
+                    <button type="button"  title="{{__('admin.delete')}}" class="btn btn-icon btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{$row->id }}">
                       <i class="fas fa-trash-alt"></i>
                     </button>
                     <!-- Include Delete modal -->

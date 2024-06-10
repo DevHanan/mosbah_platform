1d7b002f41c622d3f288a89bea13eb56fe16add9
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-11 00:31:03 +0300

    display arabic format

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -17,7 +17,7 @@ class Blog extends Model
 
     protected  function getCustomPublishdateAttribute(){
        $formatter = new  \IntlDateFormatter('ar',  \IntlDateFormatter::NONE,  \IntlDateFormatter::SHORT, 'Asia/Riyadh',  \IntlDateFormatter::TRADITIONAL);
-       $formatter->setPattern('d MMMM yyyy - H:i'); // custom pattern
+       $formatter->setPattern('H:i - d F Y'); // custom pattern
        return $formatter->format(strtotime($this->published_at));
     }
     public function getImageFullPathAttribute($value)

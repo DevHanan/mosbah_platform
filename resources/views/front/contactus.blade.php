f40ecd38b596a8e42aee0571f823e96cad3b8fa1
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-11 01:10:54 +0300

    display arabic format

--- resources/views/front/contactus.blade.php
+++ resources/views/front/contactus.blade.php
@@ -77,7 +77,7 @@
                             <div class="row">
                                 <div class="form-group mb-3 position-relative">
                                     <i class="fa-solid fa-user secondary-color position-absolute" style="top: 13px ; right: 25px;"></i>
-                                    @if(auth()->user()->check())
+                                    @if(auth()->user())
                                     <input type="text" class="form-control pe-5 py-2" value="{{auth()->user()->name}}" name="name">
                                     @else
                                     <input type="text" class="form-control pe-5 py-2" placeholder="الاسم" name="name" required>
@@ -86,7 +86,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group mb-3 position-relative">
                                         <i class="fa-solid fa-envelope secondary-color position-absolute" style="top: 13px; right: 14px;"></i>
-                                        @if(auth()->user()->check())
+                                        @if(auth()->user())
                                         <input type="email" class="form-control pe-5 py-2" value="{{auth()->user()->email}}" name="email" readonly>
 
                                         @else
@@ -97,7 +97,7 @@
                                 <div class="col-sm-6">
                                     <div class="form-group mb-3 position-relative">
                                         <i class="fa-solid fa-phone secondary-color position-absolute" style="top: 13px; right: 17px;"></i>
-                                        @if(auth()->user()->check())
+                                        @if(auth()->user())
                                         <input type="text" class="form-control pe-5 py-2" value="{{ auth()->user()->phone }}" name="phone" readonly>
                                         @else
                                         <input type="text" class="form-control pe-5 py-2" placeholder="رقم الهاتف" name="phone" required>

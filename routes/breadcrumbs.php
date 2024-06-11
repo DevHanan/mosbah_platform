14cd0ae17189d4a58900d991abe934765c800e68
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-11 03:06:33 +0300

    custom admin logout

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -15,7 +15,7 @@ Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
 
 // Home > instructors
 Breadcrumbs::for('instructors', function (BreadcrumbTrail $trail) {
-    $trail->parent('home');
+    $trail->push(trans('navbar.Home'), route('instructor.dashboard.index'));
     // $trail->push(trans('navbar.instructors.list'), route('admin.instructors.index'));
 });
 

2650eee4604413b8785e1371274b11de18ff053c
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-24 02:59:58 +0300

    update

--- app/Http/Controllers/Front/RegisterController.php
+++ app/Http/Controllers/Front/RegisterController.php
@@ -98,7 +98,7 @@ class RegisterController extends Controller
             $expire_time = time() + $landingSetting->verification_expire_time_in_seconds;
     
             $expire_time = time() + $landingSetting->verification_expire_time_in_seconds;
-            $item = $model::where('email',$request->email)->where('verification_code',implode(' ', (array)$request->verify))->where('verification_expire_time','>=',$expire_time)->first();        
+            $item = $model::where('email',$request->email)->where('verification_code',implode(' ', (array)$request->verify))->first();        
        if($item){
         $item->verification_code = '';
         $item->verification_expire_time = '';

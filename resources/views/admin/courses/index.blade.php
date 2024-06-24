e9cf8c390c920e29ae870e34ade1f0a8b3f658cd
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-25 01:56:05 +0300

    update

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -44,19 +44,19 @@
             </div>
 
             <div class="col-md-6">
-              <p>
                 <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
                   {{__('admin.advanced_search')}}
                 </button>
-              </p>
-              <div style="min-height: 120px;">
+             
+              
+            </div>
+            <div style="min-height: 120px;">
                 <div class="collapse collapse-horizontal" id="collapseWidthExample">
                   <div class="card card-body" style="width: 300px;">
                     This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
                   </div>
                 </div>
               </div>
-            </div>
           </div>
 
           <div class="table-responsive">

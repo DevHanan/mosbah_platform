34a60bd4504756bb96277b0ce06c1f60b2fd56ed
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-26 07:29:03 +0300

    Last update

--- resources/views/admin/bankQuestions/index.blade.php
+++ resources/views/admin/bankQuestions/index.blade.php
@@ -105,11 +105,11 @@
 
                   </td>
                   <td>
-                    @if( $row->correct_answer == 1)
+                    @if( $row->correct_answer === 1)
                     {{ $row->answer1 }}
-                    @elseif( $row->correct_answer == 2)
+                    @elseif( $row->correct_answer === 2)
                     {{ $row->answer2 }}
-                    @elseif( $row->correct_answer == 3)
+                    @elseif( $row->correct_answer === 3)
                     {{ $row->answer3 }}
                     @else
                     {{ $row->answer4 }}

702e0209857be6ca54171f82338ad3076797c34d
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-06-26 03:18:06 +0300

    updates

--- resources/views/admin/instructors/show.blade.php
+++ resources/views/admin/instructors/show.blade.php
@@ -44,7 +44,8 @@
                                                         {{ $row->courseNumber }}
                                                     </div>
                                                     <div class="text-secondary">
-                                                    {{ __('admin.instructors.total_course') }}                                                    </div>
+                                                        {{ __('admin.instructors.total_course') }}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -55,22 +56,20 @@
                                         <div class="card-body" style="min-height:90px;">
                                             <div class="row align-items-center">
                                                 <div class="col-auto">
-                                                    <span class="bg-green text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-community" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
-                                                            <path d="M13 7l0 .01" />
-                                                            <path d="M17 7l0 .01" />
-                                                            <path d="M17 11l0 .01" />
-                                                            <path d="M17 15l0 .01" />
+                                                    <span class="bg-blue text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                            <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"></path>
+                                                            <path d="M12 3v3m0 12v3"></path>
                                                         </svg> </span>
                                                 </div>
                                                 <div class="col">
                                                     <div class="font-weight-medium">
-                                                        {{  $row->total_balance }} {{ $setting->currency }}
+                                                        {{ $row->total_balance }} {{ $setting->currency }}
                                                     </div>
                                                     <div class="text-secondary">
-                                                    {{ __('admin.instructors.total_balance') }}                                                    </div>
+                                                        {{ __('admin.instructors.total_balance') }}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -82,28 +81,25 @@
                                             <div class="row align-items-center">
                                                 <div class="col-auto">
                                                     <span class="bg-twitter text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/brand-twitter -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                            <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
-                                                            <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
-                                                            <path d="M3 6l0 13" />
-                                                            <path d="M12 6l0 13" />
-                                                            <path d="M21 6l0 13" />
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                            <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"></path>
+                                                            <path d="M12 3v3m0 12v3"></path>
                                                         </svg> </span>
                                                 </div>
                                                 <div class="col">
                                                     <div class="font-weight-medium">
-                                                    <td>{{ $row->current_balance }} {{ $setting->currency }}</td>
+                                                        <td>{{ $row->current_balance }} {{ $setting->currency }}</td>
                                                     </div>
                                                     <div class="text-secondary">
-                                                    <th>{{ __('admin.instructors.current_balance') }}</th>
+                                                        <th>{{ __('admin.instructors.current_balance') }}</th>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                               
+
                             </div>
                         </div>
                     </div>

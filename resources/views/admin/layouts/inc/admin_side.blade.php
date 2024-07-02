4dd1ac7c572c52b1808223e86e9aa56f445e4313
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-02 20:47:39 +0300

    updates

--- resources/views/admin/layouts/inc/admin_side.blade.php
+++ resources/views/admin/layouts/inc/admin_side.blade.php
@@ -373,13 +373,13 @@
           <div class="dropdown-menu   ">
             <div class="dropdown-menu-columns">
               <div class="dropdown-menu-column">
-                <a class="dropdown-item  @if(request()->routeIs(['admin.subjects.*'])) active @endif" href="{{url('admin/subjects?classroom=1')}}">
+                <a class="dropdown-item  @if(request()->routeIs('admin/subjects?classroom=1')) active @endif" href="{{url('admin/subjects?classroom=1')}}">
                   {{ __('navbar.calculate_equation.first_secondary_subjects') }}
                 </a>
-                <a class="dropdown-item @if(request()->routeIs(['admin.subjects.*'])) active @endif" href="{{url('admin/subjects?classroom=2')}}">
+                <a class="dropdown-item @if(request()->url('admin/subjects?classroom=2')) active @endif" href="{{url('admin/subjects?classroom=2')}}">
                   {{ __('navbar.calculate_equation.second_secondary_subjects') }}
                 </a>
-                <a class="dropdown-item @if(request()->routeIs(['admin.subjects.*'])) active @endif" href="{{url('admin/subjects?classroom=3')}}">
+                <a class="dropdown-item @if(request()->url('admin/subjects?classroom=3'])) active @endif" href="{{url('admin/subjects?classroom=3')}}">
                   {{ __('navbar.calculate_equation.third_secondary_subjects') }}
                 </a>
                 <!-- <a class="dropdown-item @if(request()->routeIs(['admin.subjects.*'])) active @endif" href="{{url('admin/subjects?classroom=3')}}">

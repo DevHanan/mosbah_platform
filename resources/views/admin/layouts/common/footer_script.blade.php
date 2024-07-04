73b12f47f89a99c9459ef01a87c47037a86fb14d
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-04 21:44:37 +0300

    update datatable configuration

--- resources/views/admin/layouts/common/footer_script.blade.php
+++ resources/views/admin/layouts/common/footer_script.blade.php
@@ -442,6 +442,16 @@
 <script type="text/javascript" src="https://cdn.datatables.net/colvis/1.1.2/js/dataTables.colVis.min.js"></script>
 
 
+
+<script type="text/javascript" src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.dataTables.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.colVis.min.js"></script>
+
 <script type="text/javascript">
 	$(document).ready(function() {
 		  $('.datatable').DataTable({

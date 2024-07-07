71b28bc9697b3e036462e6bf8c1a0ed5f230eb84
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-07 11:28:37 +0300

    debug

--- resources/views/admin/layouts/inc/admin_side.blade.php
+++ resources/views/admin/layouts/inc/admin_side.blade.php
@@ -358,7 +358,7 @@
         </li>
 
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle @if(request()->routeIs(['admin.subjects.*'])) show @endif " href="#navbar-layout" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="false">
+          <a class="nav-link dropdown-toggle @if(request()->routeIs(['admin.subjects.*','admin.faculities.*'])) show @endif " href="#navbar-layout" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="false">
             <span class="nav-link-icon d-md-none d-lg-inline-block">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-question-mark">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -370,7 +370,7 @@
               {{ __('navbar.calculate_equation.management') }}
             </span>
           </a>
-          <div class="dropdown-menu  @if(request()->routeIs(['admin.subjects.*'])) show @endif  ">
+          <div class="dropdown-menu  @if(request()->routeIs(['admin.subjects.*','admin.faculities.*'])) show @endif  ">
             <div class="dropdown-menu-columns">
               <div class="dropdown-menu-column">
                 <a class="dropdown-item  @if(request()->routeIs(['admin.subjects.firstsubjects'])) active @endif" href="{{url('admin/subjects?classroom=1')}}">

affd79523d43890456d279034e881fc0b2b335f2
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-07 11:38:13 +0300

    debug

--- resources/views/admin/finicals/courseprofit.blade.php
+++ resources/views/admin/finicals/courseprofit.blade.php
@@ -30,7 +30,7 @@
           </div>
 
           <div class="table-responsive">
-          <table id="basic-table" class="display table nowrap table-striped table-hover" style="width:100%">
+          <table id="coursesProfit" class="display table nowrap table-striped table-hover" style="width:100%">
           <thead>
                 <tr>
                   <th class="w-1"><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices"></th>
@@ -105,4 +105,84 @@
   </div>
 </div>
 
+<?php 
+if(app()->getLocale() == 'ar'){
+$locale = 'Arabic';
+$dir = 'right to left';
+}
+else
+{ 
+$locale = 'English';
+$dir = 'left to right';
+}
+
+?>
 @endsection
+
+@push('scripts')
+<script>
+
+let locale = '<?= $locale?>'; // assuming this is set by your PHP code
+let url = `https://cdn.datatables.net/plug-ins/1.10.24/i18n/${locale}.json`;
+let dir = '<?= $dir?>'; 
+console.log(url);
+
+new DataTable('#coursesProfit', {
+  language: {
+
+    url: url
+  },
+  'direction': dir,
+  columnDefs: [
+                      {className: 'dt-center', targets: '_all' ,
+
+                      }
+                        ],
+    layout: {
+        topStart: {
+            buttons: [
+              {
+                    extend: 'colvis',
+                    text: '<i class="fa fa-eye-slash text-primary" aria-hidden="true" style="font-size:large;"></i>',
+                    
+                    columns: ":not(':first')"
+                  },
+                  
+                {
+                    extend: 'copyHtml5',
+                    text: '<i class="fas fa-copy text-primary" style="font-size:large;"></i>',
+                    exportOptions: {
+                      columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    text: '<i class="fas fa-file-excel text-primary" style="font-size:large;"></i>',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: '<i class="far fa-file-pdf fa-lg text-primary"></i>',
+                    exportOptions: {
+                      columns: ':visible'
+                    }
+                },
+                {
+                        extend: 'csvHtml5',
+						title: 'CSV',
+                        text: '<i class="fas fa-file text-primary" style="font-size:large;"></i>',
+                        exportOptions: {
+                            columns: ':not(:last-child)',
+                            columns: ':visible'
+
+                        }
+                    },
+               
+            ]
+        }
+    }
+});
+</script>
+@endpush

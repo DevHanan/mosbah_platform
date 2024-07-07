3c06b8ebcb6c2248b69471d99a71dcf5f82c39a6
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-07 14:19:59 +0300

    debug

--- resources/views/front/blogs.blade.php
+++ resources/views/front/blogs.blade.php
@@ -12,7 +12,8 @@
                 <span>من</span> <input type="date" class="form-control mx-2 mb-2" name="start"> 
                 <span>الى</span> <input type="date" class="form-control mx-2 mb-2" name="end"> 
                 <button type="submit" class="btn border p-2 px-4 mx-2 mb-2 reset_btn"> بحث </button>
-              </div>
+                <a href="{{url('/blogs')}}" class="btn border p-2 px-4 mx-2 mb-2 reset_btn">تحميل</a>
+                </div>
               </form>
             </div>
           </section>
@@ -24,9 +25,9 @@
                     <div class="card shadow-sm mb-5 mx-3">
                         <a href="{{url('/blog/'.$blog->id)}}" class="position-relative">
                             @if($blog->main_image)
-                            <img class="card-img-top w-100 p-2" src="{{ asset($blog->imageFullPath)}}" style="height: 200px;" alt="Card image cap">
+                            <img class="card-img-top w-100 p-2" src="{{ asset($blog->imageFullPath)}}" style="height: 250px;" alt="Card image cap">
                             @else
-                            <img class="card-img-top w-100 p-2" src="{{asset('front/img/Book.svg')}}" style="height: 200px;" alt="Card image cap">
+                            <img class="card-img-top w-100 p-2" src="{{asset('front/img/Book.svg')}}" style="height: 250px;" alt="Card image cap">
                             @endif
                         </a>
                         <div class="card-body">

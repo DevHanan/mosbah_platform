c97136f93363f579d5095f3c341abf5973515077
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-08 17:04:09 +0300

    update vaildation

--- resources/views/admin/mails/index.blade.php
+++ resources/views/admin/mails/index.blade.php
@@ -10,19 +10,7 @@
         {{ Breadcrumbs::render('tracks') }}
 
       </div>
-      <div class="col-auto ms-auto d-print-none">
-        <div class="btn-list">
-
-          <a href="{{ route($route.'.create') }}" class="btn btn-primary d-none d-sm-inline-block">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 5l0 14" />
-              <path d="M5 12l14 0" />
-            </svg>
-            {{__('admin.btn_add_new')}} </a>
-
-        </div>
-      </div>
+   
       <!-- Page title actions -->
     </div>
   </div>
@@ -78,12 +66,6 @@
 
 
                   <td style="width: 270px;">
-
-
-                    <a href="{{ route($route.'.edit',$row->id) }}"   title="{{__('admin.edit')}}"  data-bs-toggle="tooltip" data-bs-placement="bottom" class="btn btn-icon btn-primary btn-sm" data-title="{{__('admin.edit')}}">
-                      <span class="far fa-edit "></span>
-                    </a>
-
                    
 
                     <button type="button"  title="{{__('admin.delete')}}"   data-bs-placement="bottom" class="btn btn-icon btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{$row->id }}">

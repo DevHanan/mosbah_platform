d0ad7028776ec8b53382895482b17fb62fcd0263
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-08 20:08:29 +0300

    ed

--- resources/views/admin/layouts/inc/admin_side.blade.php
+++ resources/views/admin/layouts/inc/admin_side.blade.php
@@ -396,7 +396,7 @@
         </li>
 
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle @if(request()->routeIs(['admin.certifications.index','admin.studentscertifications','admin.externelstudentscertifications'])) show @endif" href="#navbar-layout" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="false">
+          <a class="nav-link dropdown-toggle @if(request()->routeIs(['admin.grantingcertificate','admin.certifications.index','admin.studentscertifications','admin.externelstudentscertifications'])) show @endif" href="#navbar-layout" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="false">
             <span class="nav-link-icon d-md-none d-lg-inline-block">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-certificate">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -412,7 +412,7 @@
               {{ __('navbar.certifications.certifications_management') }}
             </span>
           </a>
-          <div class="dropdown-menu  @if(request()->routeIs(['admin.certifications.index','admin.studentscertifications','admin.externelstudentscertifications'])) show @endif">
+          <div class="dropdown-menu  @if(request()->routeIs(['admin.grantingcertificate','admin.certifications.index','admin.studentscertifications','admin.externelstudentscertifications'])) show @endif">
             <div class="dropdown-menu-columns">
               <div class="dropdown-menu-column">
                 <a class="dropdown-item @if(request()->routeIs(['admin.certifications.index'])) active @endif" href="{{url('admin/certifications')}}">

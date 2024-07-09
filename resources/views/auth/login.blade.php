2fc1dfde6975488fa258fe94528ed865ca816d2f
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-09 14:25:55 +0300

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
                         @enderror
                     </div>
              
-                    < class="input-group mb-4 password-container">
+                    <div class="input-group mb-4 password-container">
                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="كلمة المرور">
                         <span class="password-toggle-icon">
                         <i class="fa-solid fa-eye" id="eye"></i></span>

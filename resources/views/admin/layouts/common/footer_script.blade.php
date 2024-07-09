e6ab5bd60f1638f2e004dee6bdf43d042f86281e
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-09 15:14:42 +0300

    updates

--- resources/views/admin/layouts/common/footer_script.blade.php
+++ resources/views/admin/layouts/common/footer_script.blade.php
@@ -702,17 +702,16 @@
 	$('document').ready(function() {
 		const passwordField = document.getElementById("password");
 		const togglePassword = document.querySelector(".password-toggle-icon");
-		const togglePasswordicon =  document.querySelector(".password-icon");
 
 		togglePassword.addEventListener("click", function() {
 			if (passwordField.type === "password") {
 				passwordField.type = "text";
-				// togglePassword.classList.remove("fa-eye");
-				togglePasswordicon.classList.add("fa-eye-slash");
+				togglePassword.classList.remove("fa-eye");
+				togglePassword.classList.add("fa-eye-slash");
 			} else {
 				passwordField.type = "password";
-				// togglePassword.classList.remove("fa-eye-slash");
-				togglePasswordicon.classList.add("fa-eye");
+				togglePassword.classList.remove("fa-eye-slash");
+				togglePassword.classList.add("fa-eye");
 			}
 		});
 	});

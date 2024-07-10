0fe5227c3d33f84e7ca98aa405cb6ae3936a6edf
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-10 06:14:32 +0300

    format coding

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -54,8 +54,9 @@
 
                                 <div class="mb-3">
                                     <label class="form-label" for="phone">{{ __('admin.students.phone_number') }} <span>*</span></label>
-                                    <input type="text" class="form-control" name="phone" id="phone" value="{{ old('phone') }}">
+                                    <input type="tel" class="form-control" name="phone" id="phone" value="{{ old('phone') }}">
 
+                                    
 
                                     @error('phone')
                                     <div class="invalid-feedback">

2f2169e478d200c47182a2d7f1c6292c0eb81999
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-11 02:10:23 +0300

    format coding

--- resources/views/admin/layouts/common/footer_script.blade.php
+++ resources/views/admin/layouts/common/footer_script.blade.php
@@ -732,9 +732,9 @@
 	});
 </script>
 
-<script>
+<!-- <script>
      const phoneInputField = document.querySelector("#phone");
         const phoneInput = window.intlTelInput(phoneInputField, {
             utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
         });
- </script>
+ </script> -->

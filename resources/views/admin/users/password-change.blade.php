84c20218a687a0ead1ea78e25949500207a8d79d
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-07-11 02:17:16 +0300

    format coding

--- resources/views/admin/users/password-change.blade.php
+++ resources/views/admin/users/password-change.blade.php
@@ -2,7 +2,7 @@
     <div id="changePasswordModal-{{ $row->id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-              <form class="needs-validation" novalidate action="{{ route($route.'-password-change') }}" method="post" enctype="multipart/form-data">
+              <form class="needs-validation"data-remote="true"  action="{{ route($route.'-password-change') }}" method="post" enctype="multipart/form-data">
                 @csrf
 
                 <div class="modal-header">
@@ -16,7 +16,6 @@
                     <div class="form-group">
                         <label for="password" class="form-label">{{ __('admin.users.field_password') }} <span>*</span></label>
                         <input type="password" class="form-control" name="password" id="password" value="" required autocomplete="new-password">
-                        <span class="fa password-toggle-icon eye-icon ">
                         <i class="fa password-icon" aria-hidden="true"></i>
                             @error('password')
                         <div class="invalid-feedback">
@@ -29,7 +28,6 @@
                         <label for="password-confirm" class="form-label">
                         {{ __('admin.users.field_password_confirmation') }}                             <span>*</span></label>
                         <input type="password" class="form-control" name="password_confirmation" id="password-confirm" value="" required autocomplete="new-password">
-                        <span class="fa password-toggle-icon eye-icon ">
                         <i class="fa password-icon" aria-hidden="true"></i>
                         @error('password_confirmation')
                         <div class="invalid-feedback">
@@ -48,3 +46,14 @@
             </div>
         </div>
     </div>
+
+    <script>
+  $('document').ready(function(){
+    $('form[data-remote]').on('ajax:error', function(e, xhr, status, error) {
+    var errorMessage = xhr.responseText;
+    // popup a modal with the error message
+    $('#changePasswordModal-<?php $row->id ?>').modal('show').find('.modal-body').text(errorMessage);
+});
+  });
+  
+</script>
